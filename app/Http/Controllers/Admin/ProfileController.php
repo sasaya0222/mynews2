e167b175fb28fb1f05f4c7c1db62a9dc21f784a0
”名前” <>
2019-11-07 16:19:52 +0000

    １６

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -12,9 +12,34 @@ class ProfileController extends Controller
   {
       return view('admin.profile.create');
   }
-  public function create()
+ public function create(Request $request)
   {
-      return redirect('admin/profile/create');
+
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, News::$rules);
+
+      $news = new News;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+
+      return redirect('admin/news/create');
   }
 
  public function edit()
