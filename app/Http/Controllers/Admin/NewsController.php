2af2a9adffe8c2f73e5c95b95929a23643c08276
”名前” <>
2019-11-08 08:13:39 +0000

    18まで表示されず

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
+use App\History;
+use Carbon\Carbon;
 
 
 class NewsController extends Controller
@@ -52,32 +54,46 @@ class NewsController extends Controller
       return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
   }
 
+  // 以下を追記
 
   public function edit(Request $request)
   {
       // News Modelからデータを取得する
       $news = News::find($request->id);
-
+      if (empty($news)) {
+        abort(404);    
+      }
       return view('admin.news.edit', ['news_form' => $news]);
   }
 
 
-  public function update(Request $request)
-  {
-      // Validationをかける
-      $this->validate($request, News::$rules);
-      // News Modelからデータを取得する
-      $news = News::find($request->id);
-      // 送信されてきたフォームデータを格納する
-      $news_form = $request->all();
-      unset($news_form['_token']);
+   public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        $news = News::find($request->input('id'));
+        $news_form = $request->all();
+        if ($request->input('remove')) {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
 
-      // 該当するデータを上書きして保存する
-      $news->fill($news_form)->save();
+        unset($news_form['_token']);
+        unset($news_form['image']);
+        unset($news_form['remove']);
+        $news->fill($news_form)->save();
 
-      return redirect('admin/news/');
-  }
+        // 以下を追記
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
 
+        return redirect('admin/news/');
+    }
   // 以下を追記　　
   public function delete(Request $request)
   {
@@ -87,6 +103,4 @@ class NewsController extends Controller
       $news->delete();
       return redirect('admin/news/');
   }  
-
-
 }
